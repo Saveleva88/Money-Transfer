bbb97e3014b1768ab870f615cf8602126d904309
Irina Saveleva <fomina_88@mail.ru>
2021-10-02 12:26:10 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+        public static void main(String[] args) {
+            int currentAccountBalance = 2_000_000_000;
+            int transferAmount = 500_000_000;
+            int totalSum = currentAccountBalance + transferAmount;
+            System.out.println(totalSum);
+
+        }
+    }
